e02be3131b4456551e565f5be1ec42ec267fccee
SuminaAnastasya95 <suminan1995@gmail.com>
2025-11-16 00:42:50 +0300

    new file

--- cycles/game.py
+++ cycles/game.py
@@ -6,22 +6,27 @@ pc_score = 0
 user_score = 0
 for r in range(1, round_count + 1):
     print(f"\nРайунд{r}")
-    selection = input("Что выбираете (камень/ ножницы/ бумага):  ")
-    if selection not in pc:
-        print("Не корректный ввод")
-        exit()
-    pc_select = random.choice(pc)
-    print(f"Компьтер выбрал: {pc_select}")
-    if selection ==  pc_select:
-        print('ничья')
-    elif (selection == "камень" and pc_select == "ножницы") or \
-          (pc_select == "камень" and selection == "бумага") or \
-            (selection == "ножницы" and pc_select == "бумага"):
-        print("Выйграл человек")
-        user_score += 1
-    else:
-        print("Выйграл ПК")
-        pc_score += 1
+    selection = input("Что выбираете (камень/ ножницы/ бумага): ")
+
+    while True:
+
+        if selection not in pc:
+            print("Попробуй еще раз")
+            continue
+        pc_select = random.choice(pc)
+        print(f"Компьтер выбрал: {pc_select}")
+
+        if selection ==  pc_select:
+            print('ничья')
+        elif (selection == "камень" and pc_select == "ножницы") or \
+            (pc_select == "камень" and selection == "бумага") or \
+                (selection == "ножницы" and pc_select == "бумага"):
+            print("Выйграл человек")
+            user_score += 1
+        else:
+            print("Выйграл ПК")
+            pc_score += 1
+            break
 print("===Итог игры====")
 print(f"Счет человека: {user_score}")
 print(f"Счет компьютера: {pc_score}")

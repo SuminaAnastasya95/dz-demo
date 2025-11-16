6736757062df5855eebfab75d45121983bb1c254
SuminaAnastasya95 <suminan1995@gmail.com>
2025-11-16 19:37:40 +0300

    work with functions

--- cycles/game.py
+++ cycles/game.py
@@ -1,39 +1,53 @@
 import random
 
+
+def select_variant():
+    selection = None
+    while selection not in pc:
+        selection = input("Что выбираете (камень/ ножницы/ бумага): ")
+        if selection not in pc:
+            print("Попробуй еще раз")
+    return selection
+
+
+def compute_game_result(user_ch: str, comp_ch: str):
+    if user_ch == comp_ch:
+        print('ничья')
+        return (0, 0)
+    elif (user_ch == "камень" and comp_ch == "ножницы") or \
+        (comp_ch == "камень" and user_ch == "бумага") or \
+            (user_ch == "ножницы" and comp_ch == "бумага"):
+        print("Выйграл человек")
+        return (1, 0)
+    else:
+        print("Выйграл ПК")
+        return (0, 1)
+
+
+def print_result(user_result: int, pc_result: int):
+    print("===Итог игры====")
+    print(f"Счет человека: {user_result}")
+    print(f"Счет компьютера: {pc_result}")
+    if user_result > pc_result:
+        print("Ты победил!")
+    if user_result < pc_result:
+        print("ПК победил!")
+    else:
+        print("Ничья!")
+
+
 round_count = int(input("Введите число раунодов: "))
 pc = ("камень", "ножницы", "бумага")
 pc_score = 0
 user_score = 0
+
 for r in range(1, round_count + 1):
     print(f"\nРайунд{r}")
-    selection = input("Что выбираете (камень/ ножницы/ бумага): ")
+    selection = select_variant()
+    pc_select = random.choice(pc)
+    print(f"Компьтер выбрал: {pc_select}")
+    [user_mod, comp_mod] = compute_game_result(selection, pc_select)
+    user_score += user_mod
+    pc_score += comp_mod
 
-    while True:
-
-        if selection not in pc:
-            print("Попробуй еще раз")
-            continue
-        pc_select = random.choice(pc)
-        print(f"Компьтер выбрал: {pc_select}")
-
-        if selection ==  pc_select:
-            print('ничья')
-        elif (selection == "камень" and pc_select == "ножницы") or \
-            (pc_select == "камень" and selection == "бумага") or \
-                (selection == "ножницы" and pc_select == "бумага"):
-            print("Выйграл человек")
-            user_score += 1
-        else:
-            print("Выйграл ПК")
-            pc_score += 1
-            break
-print("===Итог игры====")
-print(f"Счет человека: {user_score}")
-print(f"Счет компьютера: {pc_score}")
-
-if user_score > pc_score:
-    print("Ты победил!")
-if user_score < pc_score:
-    print("ПК победил!")
-else:
-    print("Ничья!")
+print_result(user_score, pc_score)

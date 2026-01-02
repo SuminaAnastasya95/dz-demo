3e9dff1b15d7ce08f6d4cc85dab02f253607abcf
SuminaAnastasya95 <suminan1995@gmail.com>
2026-01-03 01:09:53 +0300

    Практика sorted

---
+++ test.py
@@ -0,0 +1,13 @@
+number1 = 17
+number2 = 24
+number3 = 33
+
+is_even1 = number1 % 2 == 0
+
+# is_odd1: True
+# is_even2: True
+# is_odd2: False
+# is_even3: False
+# is_odd3: True
+
+print("is_even1:", is_even1)

db2e36817eb785f7c133dbb9fd283e5d075a9e2d
SuminaAnastasya95 <suminan1995@gmail.com>
2026-01-18 20:04:27 +0300

    Проект трекинга

--- menu/main.py
+++ menu/main.py
@@ -1,3 +1,6 @@
+from command.help_menu import help_commands
+
+
 def main():
     print("Таск менеджер. help - для справки")
     while True:
@@ -7,6 +10,10 @@ def main():
             cmd, args = parts[0], parts[1:]
             match cmd:
                 case "help":
+                    kind_help = input(
+                        "С какой командой помочь?\nlist\nadd\nremove\nedit\ntags\nexit\n>>")
+                    help_commands(kind_help)
+                case "list":
                     pass
                 case "add":
                     pass
